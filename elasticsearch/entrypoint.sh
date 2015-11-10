76f4092099fa2897f45998256eb8124db1e26d8c
Joshua Rich <joshua.rich@elastic.co>
2015-11-10 16:39:57 +1100

    Fix entrypoint script to support passing command-line options to Elasticsearch.

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$1" = 'elasticsearch' ]; then
+if [[ "$1" = 'elasticsearch' ]]; then
+    shift
     chown -R $ES_USER:root /opt/elasticsearch/{config,data,plugins,logs}
-    exec /usr/local/bin/gosu $ES_USER /opt/elasticsearch/bin/elasticsearch
+    exec /usr/local/bin/gosu $ES_USER /opt/elasticsearch/bin/elasticsearch "$@"
 fi
 
-exec "$@"
+exec exec /usr/local/bin/gosu $ES_USER "$@"
