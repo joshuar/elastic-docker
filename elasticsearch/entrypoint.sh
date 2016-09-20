73b71e0275a78449d20c76447f059b0ea1d1f369
Joshua Rich <joshua.rich@elastic.co>
2016-09-20 15:42:47 +1000

    Slight changes.

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -5,12 +5,12 @@ if [[ "$1" = 'elasticsearch' ]]; then
     shift
     exec /usr/local/bin/gosu elasticsearch \
 	 /usr/share/elasticsearch/bin/elasticsearch \
+	 -Des.bootstrap.seccomp=false \
 	 -Des.pidfile="${ES_PID_DIR}/elasticsearch.pid" \
 	 -Des.default.path.home="${ES_HOME}" \
 	 -Des.default.path.logs="${ES_LOG_DIR}" \
 	 -Des.default.path.data="${ES_DATA_DIR}" \
-	 -Des.default.path.conf="${ES_CONF_DIR}" \
-	 "$@"
+	 -Des.default.path.conf="${ES_CONF_DIR}"
 fi
 
 exec /usr/local/bin/gosu root "$@"
