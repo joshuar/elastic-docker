a9e063493685f87b9454d154ac2a21ee60cf05b8
Joshua Rich <joshua.rich@gmail.com>
2017-01-08 14:51:59 +1100

    Adjust Elasticsearch v5.

--- elasticsearch/entrypoint5.sh
+++ elasticsearch/entrypoint5.sh
@@ -3,13 +3,9 @@ set -e
 
 if [[ "$1" = 'elasticsearch' ]]; then
     shift
-	chown -R elasticsearch:elasticsearch ${ES_DATA_DIR}
     exec /usr/local/bin/gosu elasticsearch \
 	 ${ES_HOME}/bin/elasticsearch \
-	 -Ebootstrap.seccomp=false \
-	 -Epath.logs="${ES_LOG_DIR}" \
-	 -Epath.data="${ES_DATA_DIR}" \
-	 -Epath.conf="${ES_CONF_DIR}"
+	 -Ebootstrap.seccomp=false
 fi
 
 exec /usr/local/bin/gosu root "$@"
